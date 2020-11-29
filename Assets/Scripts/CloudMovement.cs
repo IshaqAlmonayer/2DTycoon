f2c61d2026947bfbc4054f31952888b3f9613cbc
unknown <ishaqalmouner@gmail.com>
2020-11-29 18:17:37 +0200

    Changed background and some UI features

---
+++ Assets/Scripts/CloudMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private Vector2 _movement;
+    private float _direction = 1;
+
+    public float speed = 2f;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _movement = new Vector2(_direction, 0f);
+    }
+
+    void FixedUpdate()
+    {
+        //movement
+        float HorizontalVelocity = _movement.normalized.x * speed;
+        _rigidbody.velocity = new Vector2(HorizontalVelocity, _rigidbody.velocity.y);
+    }
+
+}
---
+++ Assets/Scripts/CloudSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    public GameObject Cloud;
+    public float _minSpawnTime;
+    public float _maxSpawnTime;
+
+    private float _timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_timer > 0)
+            _timer -= Time.deltaTime;
+        else
+        {
+            Instantiate(Cloud, transform.position, Quaternion.identity);
+
+            _timer += Random.Range(_minSpawnTime, _maxSpawnTime);
+        }
+    }
+}
