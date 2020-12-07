827204b3a8aee04b249b208bd6ef667382e2436d
unknown <ishaqalmouner@gmail.com>
2020-12-07 10:52:57 +0200

    Added More Info for Shops

---
+++ Assets/Scripts/OnMouseHoldButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class OnMouseHoldButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    public GameObject Panel;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(this.gameObject.name + " Was Clicked.");
+        Panel.SetActive(true);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log(this.gameObject.name + " Was Released.");
+        Panel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/ShopOnHoldInfoButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopOnHoldInfoButton : MonoBehaviour
+{
+    public Stand stand;
+    public Text text;
+    public string InfoType;
+
+    void Start()
+    {
+        if(InfoType == "Price")
+            text.text = stand.GetComponent<Stand>().productPrice.ToString() + "$";
+        if(InfoType == "WaitingTime")
+            text.text = stand.GetComponent<Stand>().StandWaitingTime.ToString() + " sec";
+    }
+
+    private void Update()
+    {
+        if (InfoType == "Price")
+            text.text = stand.GetComponent<Stand>().productPrice.ToString() + "$";
+        if (InfoType == "WaitingTime")
+            text.text = stand.GetComponent<Stand>().StandWaitingTime.ToString() + " sec";
+    }
+}
