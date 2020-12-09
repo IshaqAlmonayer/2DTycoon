23bd1ec6190b4301d358f3a6337fe3fa02f68780
unknown <ishaqalmouner@gmail.com>
2020-12-09 13:57:55 +0200

    Added Transitions

--- Assets/Scripts/LoadSceneButton.cs
+++ Assets/Scripts/LoadSceneButton.cs
@@ -8,9 +8,10 @@ using System.IO;
 
 public class LoadSceneButton : MonoBehaviour
 {
-
+    public Animator transition;
     public UnityEngine.UI.Button Button;
     public int sceneNo;
+    private float waitTime = 1f;
 
     void Start()
     {
@@ -19,32 +20,44 @@ public class LoadSceneButton : MonoBehaviour
 
     public void TaskOnClick()
     {
+        
         switch (sceneNo)
         {
             case 1:
+                Debug.Log("Case 1");
                 string path = Application.persistentDataPath + "/GameDataMap1.lol";
                 if (File.Exists(path))
                 {
                     //Load first map
-                    SceneManager.LoadScene(2);
+                    StartCoroutine(LoadLevel(2));
                 }
                 else
                 {
                     //Load Tutorial
-                    SceneManager.LoadScene(1);
+                    StartCoroutine(LoadLevel(1));
                 }
                 break;
             case 2:
+                Debug.Log("Case 2");
                 //Load first map
-                SceneManager.LoadScene(2);
+                StartCoroutine(LoadLevel(2));
                 break;
             case 3:
+                Debug.Log("Case 3");
                 //load second map
-                SceneManager.LoadScene(3);
+                StartCoroutine(LoadLevel(3));
                 break;
         }
-       
-            
-
     }
+
+    IEnumerator LoadLevel(int SceneNumber)
+    {
+        Debug.Log("inside Ienumerator");
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(waitTime);
+
+        SceneManager.LoadScene(SceneNumber);
+    }
+
 }
